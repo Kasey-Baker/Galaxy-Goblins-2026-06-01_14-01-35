ef64e3b001f6ffc0f5543ac786eceb11fc4337d4
Oscar J Ramirez feliciano <OJRamirezFeliciano@student.fullsail.edu>
2026-06-09 15:39:21 -0400

    level up and bullet upgrade base setup

---
+++ Assets/Scripts/Player/LevelSystem.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class LevelSystem : MonoBehaviour
+{
+    public int currentScore = 0;
+    public int currentLevel = 1;
+    public int[] scoreThresholds = { 1000, 2500, 5000 };
+
+    public int bulletdamage = 10;
+    public int bulletAmount = 1;
+
+    public void AddScore(int points)
+    {
+        currentScore += points;
+
+        CheckForLevelUp();
+
+    }
+    private void CheckForLevelUp()
+    {
+        if (currentLevel <= scoreThresholds.Length)
+        {
+            int requierdScore=scoreThresholds[currentLevel-1];
+
+            if (currentScore >= requierdScore)
+            {
+                LevelUp();
+            }
+           
+
+
+        }
+
+    }
+
+    private void LevelUp()
+    {
+        currentLevel++;
+        bulletdamage += 5;
+        bulletAmount += 1;
+
+    }
+
+}
